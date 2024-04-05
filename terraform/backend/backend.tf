e7beba00fdac347258d5b67d688038bc1c82676f
aut0m4t0r <aut0m4t0r@outlook.com>
2024-04-05 15:05:02 -0400

    Corrected an issue with terraform backend state management due to separation of concerns terraform directory. Added a gitignore to prevent the terraform registry files from being added to git.

--- terraform/backend/backend.tf
+++ terraform/backend/backend.tf
@@ -1,7 +1,6 @@
 terraform {
   backend "s3" {
     bucket         = "interview-s3-state-bucket"
-    key            = "state/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "interview-dynamo-lock-table"
     encrypt        = true
